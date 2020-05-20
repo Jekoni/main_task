06796c7a8e08f462c6ec3561c53e8bc571323fd1
ekarimova <ekarimova@kmis.ru>
2020-05-20 15:46:31 +0300

    Nasledov

--- pages/product_page.py
+++ pages/product_page.py
@@ -19,3 +19,9 @@ class PageObject(BasePage):
         book_price = self.browser.find_element(*ProductPageLocators.BOOK_PRICE).text
         basket_price = self.browser.find_element(*ProductPageLocators.BASKET_PRICE).text
         assert book_price == basket_price, "Price is different"
+		
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE),"Success message is presented, but should not be"
+		
+    def element_disappeared(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE),"Success message is presented, but should not be"
--- test_product_page.py
+++ test_product_page.py
@@ -3,18 +3,8 @@ from .pages.login_page import LoginPage
 import time
 import pytest
 
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-def test_guest_can_add_product_to_basket(browser, link):
-    #link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
     product_page = PageObject(browser, link)
     product_page.open()
     product_page.add_to_basket()
@@ -22,3 +12,38 @@ def test_guest_can_add_product_to_basket(browser, link):
     product_page.should_be_book_name()
     product_page.should_be_book_price()
     time.sleep(2)
+	
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    product_page = PageObject(browser, link)
+    product_page.open()
+    product_page.add_to_basket()
+    product_page.should_not_be_success_message()
+    time.sleep(2)
+	
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+    product_page = PageObject(browser, link)
+    product_page.open()
+    product_page.should_not_be_success_message()
+    time.sleep(2)
+
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+    product_page = PageObject(browser, link)
+    product_page.open()
+    product_page.add_to_basket()
+    product_page.element_disappeared()
+    time.sleep(2)
+	
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = PageObject(browser, link)
+    page.open()
+    page.should_be_login_link()
+	
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = PageObject(browser, link)
+    page.open()
+    page.go_to_login_page()
